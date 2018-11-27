ebf4a36f7667cf9daa2be05c556e9f503a9ef2d5
Odell <odell.purifoy@gmail.com>
2018-11-27 13:11:33 -0500

    Added styles for show cigar page and activated sell cigar link

--- app/models/cigar.rb
+++ app/models/cigar.rb
@@ -6,7 +6,7 @@ class Cigar < ApplicationRecord
 
   validates :brand, :origin, :size, :shape, :binder, :filler, :wrapper, :price, presence: true
   validates :description, presence: true, length: { maximum: 255, too_long: '%{count} characters is the maximum allowed.' }
-  validates :price, numericality: { only_integer: true }, length: { maximum: 7 }
+  validates :price, length: { maximum: 7 }
 
   BRAND = ['Drew Estate', 'Padron', 'My Father', 'Oliva', 'Punch', 'Cohiba', 'Monticristo', 'Roma Craft', 'Davidoff', 'Illusione'].freeze
   WRAPPER = %w[Candela Connecticut Maduro Natural].freeze
